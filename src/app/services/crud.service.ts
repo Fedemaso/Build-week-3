316c5cd6b90b6dd4d420ec1b2dbded4b80386d04
fedemaso <fede.maso1989@gmail.com>
2023-08-22 11:38:35 +0200

    modifiche cards

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { IExperience } from '../interfaces/iexperience';
 
+
 @Injectable({
   providedIn: 'root',
 })
